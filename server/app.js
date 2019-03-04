101236d36d7d88bbd8f38dbe17236917bc4c4dbc
Vix Morrigan <vix.morrigan@redseven.tech>
2019-03-04 14:56:16 +0000

    progression - nested data

--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-// const schema = require("./schema/schema");
+const schema = require("./schema/schema.js");
 
 const app = express();
 
-/* app.use(
+app.use(
   "/graphql",
   graphqlHTTP({
-    //schema
+    schema,
+    graphiql: true // access graphiql tool in browser
   })
-); */
+);
 
 app.listen(4000, () => {
   console.log("now listening for requests on Port 4000");
+  console.log(schema);
 });
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,21 +1,50 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList
+} = graphql;
 
-//dummy data
+// dummy data
 var books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "A Feast of Crows", genre: "Adventure", id: "2" },
-  { name: "The Long Earth", genre: "Sci-fi", id: "3" }
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "A Feast of Crows", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-fi", id: "3", authorId: "3" }
+];
+
+// further dummy data
+var authors = [
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "George R.R. Martin", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" }
 ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId });
+      }
+    }
+  })
+});
+// wrap in function to prevent undefined error
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
   })
 });
 
@@ -24,15 +53,34 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from database/other source
         return _.find(books, { id: args.id });
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       }
     }
   }
 });
 
-module.export = new GraphQLSchema({
+module.exports = new GraphQLSchema({
   query: RootQuery // how you initially jump into graph
 });
